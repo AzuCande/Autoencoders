5ca4e2c512e4e81b41e57a7940d1b0abc730251a
Azul <azkim@itba.edu.ar>
2022-11-24 08:29:51 -0300

    Actualización de README

--- README.md
+++ README.md
@@ -6,3 +6,22 @@
 * Finucci Roca, Hernán
 * Kim, Azul
 * Konfederak, Sol
+
+## Guía de instalación
+Para poder correr el proyecto es necesario tener instalado:
+* Python 3
+* NumPy
+* Matplotlib
+* Tensorflow
+* Scipy
+
+## Ejecución
+Para ejecutar el algoritmo:
+```
+python3 <exercise>
+```
+o
+```
+python <exercise>
+```
+siendo `<exercise>` es el main correspondiente al ejercicio
--- ej1_b.py
+++ ej1_b.py
@@ -12,44 +12,44 @@ x = np.array(x)
 x_mean = np.mean(x, axis=0)
 x_std = np.std(x, axis=0)
 
-# x_noise = noise(x)
-# x_noise2 = noise(x)
-# x = transform(x)
-
-x_noise = makeLittleNoise(1, x)
-x_noise2 = makeLittleNoise(1, x)
+x_noise = noise(x)
+x_noise2 = noise(x)
 x = transform(x)
-for i in range(len(x)):
-    to_predict = x_noise2[i, :]
-    pepe = x_noise[i,:]
-    graph_digits(to_predict, pepe)
 
-# layer1 = Layer(20, 35, activation="tanh")
-# layer2 = Layer(10, activation="tanh")
-# layer2_bis = Layer(5, activation="tanh")
-# layer3 = Layer(2, activation="tanh")
-# layer4 = Layer(5, activation="tanh")
-# layer4_bis = Layer(10, activation="tanh")
-# layer5 = Layer(20, activation="tanh")
-# layer6 = Layer(35, activation="tanh")
-
-# layers = [layer1, layer2, layer2_bis, layer3, layer4, layer4_bis, layer5, layer6]
-
-# encoderDecoder = MultiPerceptron(layers, init_layers=True, momentum=True, eta=0.0005)
-
-# min_error, errors, epochs, training_accuracies = encoderDecoder.train(x_noise, x, iterations_qty=10000, adaptative_eta=False)
-# print(min_error)
-
-# encoder = MultiPerceptron(encoderDecoder.neuron_layers[0:int(len(layers)/2)], init_layers=False)
-
-# decoder = MultiPerceptron(encoderDecoder.neuron_layers[int(len(layers)/2):], init_layers=False)
-
-# print("Longitud de x: " + str(len(x)))
-
+# x_noise = makeLittleNoise(1, x)
+# x_noise2 = makeLittleNoise(1, x)
+# x = transform(x)
 # for i in range(len(x)):
 #     to_predict = x_noise2[i, :]
-#     encoded = encoder.predict(to_predict)
-#     decoded = decoder.predict(encoded)
-    # graph_digits(to_predict, decoded)
+#     pepe = x_noise[i,:]
+#     graph_digits(to_predict, pepe)
+
+layer1 = Layer(20, 35, activation="tanh")
+layer2 = Layer(10, activation="tanh")
+layer2_bis = Layer(5, activation="tanh")
+layer3 = Layer(2, activation="tanh")
+layer4 = Layer(5, activation="tanh")
+layer4_bis = Layer(10, activation="tanh")
+layer5 = Layer(20, activation="tanh")
+layer6 = Layer(35, activation="tanh")
+
+layers = [layer1, layer2, layer2_bis, layer3, layer4, layer4_bis, layer5, layer6]
+
+encoderDecoder = MultiPerceptron(layers, init_layers=True, momentum=True, eta=0.0005)
+
+min_error, errors, epochs, training_accuracies = encoderDecoder.train(x_noise, x, iterations_qty=10000, adaptative_eta=False)
+print(min_error)
+
+encoder = MultiPerceptron(encoderDecoder.neuron_layers[0:int(len(layers)/2)], init_layers=False)
+
+decoder = MultiPerceptron(encoderDecoder.neuron_layers[int(len(layers)/2):], init_layers=False)
+
+print("Longitud de x: " + str(len(x)))
+
+for i in range(len(x)):
+    to_predict = x_noise2[i, :]
+    encoded = encoder.predict(to_predict)
+    decoded = decoder.predict(encoded)
+    graph_digits(to_predict, decoded)
 
 plt.show()
